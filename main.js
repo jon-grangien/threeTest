145923e13e9f6ef67ae5e6844b6b54725c85232b
jonathan <jonathan.grangien@gmail.com>
2015-03-30 23:27:19 +0200

    Clean code structure

--- main.js
+++ main.js
@@ -1,119 +1,84 @@
-  var scene, camera, renderer;
+    var scene, camera, renderer, controls;
 
-  // useful for event listeners
-  var windowHalfX = window.innerWidth / 2;
-  var windowHalfY = window.innerHeight / 2;
- 
-  init();
-  animate();
+    // useful for event listeners
+    var windowHalfX = window.innerWidth / 2;
+    var windowHalfY = window.innerHeight / 2;
 
-  console.log('hejsan :PPpppPp' );
+    init();
+    animate();
 
-  // Set up scene
-  function init() {
- 
-    container = document.getElementById( 'container' );
-    console.log(container)
+    console.log('hejsan :PPpppPp' );
 
-    // Create scene and set scene size.
-    scene = new THREE.Scene();
-    var WIDTH = window.innerWidth,
-        HEIGHT = window.innerHeight;
- 
-    // Create a renderer and add it to the DOM.
-    renderer = new THREE.WebGLRenderer({antialias:true});
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize(WIDTH, HEIGHT);
-    document.body.appendChild(renderer.domElement);
+    // Set up scene
+    function init() {
+        container = document.getElementById( 'container' );
+        console.log(container)
 
-    // Create camera, zoom out from model a bit, add it to the scene.
-    camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 20000);
-    camera.position.set(0,6,0);
-    scene.add(camera); 
-  	}
+        // Create scene and set scene size.
+        scene = new THREE.Scene();
+        var WIDTH = window.innerWidth,
+            HEIGHT = window.innerHeight;
 
-    // Create an event listener that resizes the renderer with the browser window.
-    window.addEventListener('resize', function() {
-      var WIDTH = window.innerWidth,
-          HEIGHT = window.innerHeight;
-      renderer.setSize(WIDTH, HEIGHT);
-      camera.aspect = WIDTH / HEIGHT;
-      camera.updateProjectionMatrix();
-    });
+        // Create a renderer and add it to the DOM.
+        renderer = new THREE.WebGLRenderer({antialias:true});
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize(WIDTH, HEIGHT);
+        document.body.appendChild(renderer.domElement);
 
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+        // Create camera, zoom out from model a bit, add it to the scene.
+        camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 20000);
+        camera.position.set(0,6,0);
+        scene.add(camera); 
 
-    // Create a light, set its position, and add it to the scene.
-    var light = new THREE.PointLight(0xffffff);
-    light.position.set(-100,200,100);
-    scene.add(light);
+        // Create an event listener that resizes the renderer with the browser window.
+        window.addEventListener('resize', function() {
+            var WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
+            renderer.setSize(WIDTH, HEIGHT);
+            camera.aspect = WIDTH / HEIGHT;
+            camera.updateProjectionMatrix();
+        });
 
-    // loader manager
-  //   var manager = new THREE.LoadingManager();
-  //   manager.oinProgress = function ( item, loaded, total ) {
+        document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
-		// console.log( item, loaded, total );
+        // Create a light, set its position, and add it to the scene.
+        var light = new THREE.PointLight(0xffffff);
+        light.position.set(-100,200,100);
+        scene.add(light);
 
-  //   };
-    
-    // texture
- //    var texture = new THREE.Texture();
- //    var loader = new THREE.ImageLoader( manager );
-	// loader.load( 'textures/Texture8.png', function ( image ) {
-	// 	texture.image = image;
-	// 	texture.needsUpdate = true;
-	// } );
+        var onProgress = function ( xhr ) {
+            if ( xhr.lengthComputable ) {
+                var percentComplete = xhr.loaded / xhr.total * 100;
+                console.log( Math.round(percentComplete, 2) + '% downloaded' );
+            }
+        };
 
-    var onProgress = function ( xhr ) {
-        if ( xhr.lengthComputable ) {
-          var percentComplete = xhr.loaded / xhr.total * 100;
-          console.log( Math.round(percentComplete, 2) + '% downloaded' );
-        }
-      };
-      
-    var onError = function ( xhr ) {
-    };
+        var onError = function ( xhr ) {
+        };
 
-    THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+        THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
 
-    // Load in the mesh and add it to the scene.
-    var loader = new THREE.OBJMTLLoader();
-    loader.load( "obj/Giraffe.obj", "obj/Giraffe.mtl", function(object){
-      
-    	// object.traverse( function ( child ) {
-			// if ( child instanceof THREE.Mesh ) {
-   //    			//var texture1 = new THREE.MeshLambertMaterial({color: 0x55B663});
-			// 	//child.material.map = texture1;
-			// }
-		
-    object.rotation.x = 30;
-    object.position.y = -20;
-    object.position.z = 5;
+        // Load in the mesh and add it to the scene.
+        var loader = new THREE.OBJMTLLoader();
+        loader.load( "obj/Giraffe.obj", "textures/Giraffe.mtl", function(object){ 
+            object.rotation.x = 30;
+            object.position.y = -20;
+            object.position.z = 5;
+            scene.add(object);
+        }, onProgress, onError );
 
-		scene.add(object);
-    }, onProgress, onError );
+        // Add OrbitControls so that we can pan around with the mouse.
+        controls = new THREE.OrbitControls(camera, renderer.domElement);
+    }
 
-      //mesh = new THREE.Mesh(geometry, material);
-      //scene.add(mesh);
-    // });
-
-    // Add OrbitControls so that we can pan around with the mouse.
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-    // Renders the scene and updates the render as needed.
-  	function animate() {
- 
-    // Read more about requestAnimationFrame at http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
-    requestAnimationFrame(animate);
- 
-    // Render the scene.
-    renderer.render(scene, camera);
-    // controls.update();
-  	}
+    function animate() {
+        requestAnimationFrame(animate);
+        
+        // Render scene.
+        renderer.render(scene, camera);
+        controls.update();
+    }
 
     function onDocumentMouseMove( event ) {
-
         mouseX = ( event.clientX - windowHalfX ) / 2;
         mouseY = ( event.clientY - windowHalfY ) / 2;
-
     }
